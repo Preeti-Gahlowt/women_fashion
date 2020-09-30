8bf3c6ebf9608eebc665ef6ee230604c9bf6ffb1
Preeti-Gahlowt <66962881+Preeti-Gahlowt@users.noreply.github.com>
2020-09-30 22:50:06 +0530

    Add files via upload

---
+++ view all.php
@@ -0,0 +1,152 @@
+<html>
+<head>
+<link href="css/bootstrap.min.css" rel="stylesheet"/>
+<link href="css/bootstrap-theme.min.css" rel="stylesheet"/>
+<link href="css/hover.css" rel="stylesheet" />
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/wow.min.js"></script>
+
+<style>
+.outer
+{
+min-height:3000px;
+background:white;
+}
+.out1
+{
+min-height:3250px;
+background:lightyellow;
+}
+</style>
+</head>
+<body>
+<div class="outer">
+<div class="row">
+	<div class="col-sm-12 outm">
+	<div class="row">
+		<div class="col-sm-3 out1">
+		<div class="row">
+		        <nav class=" navbar-inverse navbar-fixed-top bg-white">
+		</div>
+		</div>	
+		<div class="col-sm-9 out2">
+		<div class="row">
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/s7.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1000</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/s8.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:2000</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/s6.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:6470</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/s2.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1090</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/s4.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1800</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/s5.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1700</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/12.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1760</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/11.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1890</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/10.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1000</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/9.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:2100</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/8.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:3300</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/1.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:3200</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/s1.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1000</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/5.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1330</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/4.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:2200</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/7.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1300</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/2.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1090</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+			<div class="col-sm-3 wow fadeInDown" style="margin:20 20 20 35;  height:500px;">
+													<figure ><img style="height:370px;" class="img-fluid" alt="Responsive image" src="images/3.jpeg"/><figcaption style="height:70px;"><h3>dresswear</h3><h6>Price:1000</h6></figcaption></figure>
+													<button type="button" class="btn btn-primary btn-sm">Add Cart</button>
+													<button  type="button" style="background-color:green; color:white;"class="btn btn-secondary btn-sm">Buy Now</button> 
+							
+			</div>
+		</div>
+		</div>
+
+	</div>
+	</div>
+</div>
+</div>
+</body>
+</html>
